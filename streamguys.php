6c92a6da7b37beaabdbf1ddf49673cba3f3763b0
Eric Baker <eric@ericbaker.me>
2017-08-25 22:05:27 -0400

    Initial Commit. Nothing happens yet. The readme isn't close to done.

---
+++ streamguys.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Streamguys
+ * Plugin URI:      https://ericbaker.me/streamguys-plugin
+ * Description:     This plugin makes it easy to place your streamguys player anywhere on your WordPress site.
+ * Author:          Eric Baker
+ * Author URI:      https://ericbaker.me
+ * Text Domain:     streamguys
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Streamguys
+ */
